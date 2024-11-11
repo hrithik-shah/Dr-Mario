612b4110deb29d6fcee7a3b621df6e460e8e035b
Hrithik Shah <hrithik21.shah@gmail.com>
2024-11-11 14:50:13 -0500

    Complete milestone 2

--- project-starter-files/Milestone2.asm
+++ project-starter-files/Milestone2.asm
@@ -15,33 +15,44 @@
     .data
 ADDR_DSPL:
     .word 0x10008000
-ADDR_CAPSULE:
+CAPSULE:
     .word 0x100081ac:2      # stores the left/top position of capsule first
-ADDR_COLOR_CAPSULE:
+COLOR_CAPSULE:
     .space 8                # stores the colors of the capsule (left/top then right/bottom)
+NEXT_CAPSULE:
+    .word 0x100083e8        # stores the position of the next capsule
+NEXT_CAPSULE_COLOR:
+    .space 8                # stores the colors of the next capsule (top then bottom)
+VIRUS_POSITIONS:
+    .space 32               # stores the position of the viruses
+VIRUS_COLORS:
+    .space 32               # stores the color of the viruses
 ADDR_KBRD:
     .word 0xffff0000
 CENTR_BTTL:
     .word 0x100081ac
+TOP_LEFT_BOTTLE:
+    .word 0x1000830c
 
     .text
 	.globl 
 	
 main:
 jal initialize_capsule
+jal create_next_capsule
+jal initialize_viruses
 
 game_loop:      jal reset_screen
                 jal draw_border
+                jal draw_viruses
+                jal draw_next_capsule
                 jal draw_capsule
                 
-                # jal check_keyboard_input                # TODO
+                jal sleep
+                
                 lw $t0, ADDR_KBRD                       # $t0 = base address for keyboard
                 lw $t8, 0($t0)                          # Load first word from keyboard
                 beq $t8, 1, handle_keyboard_input       # If first word 1, key is pressed
-                
-                li $v0, 32
-                li $a0, 66
-                syscall
 
 end_game_loop: b game_loop
 
@@ -49,15 +60,17 @@ exit:
     li $v0, 10              # terminate the program gracefully
     syscall
     
-    
-############# Functions #############
+################################# Functions ##################################
+
+############################ Memory Initializers #############################
+
 
 initialize_capsule:     addi $sp, $sp, -4           # put $ra onto stack
                         sw $ra, 0($sp)
-                        jal RandomColor
+                        jal random_color
                         lw $t4, 0($sp)              # pop return value from stack
                         addi $sp, $sp, 4
-                        jal RandomColor
+                        jal random_color
                         lw $t5, 0($sp)              # pop return value from stack
                         addi $sp, $sp, 4
                         
@@ -66,8 +79,8 @@ initialize_capsule:     addi $sp, $sp, -4           # put $ra onto stack
                         
                         lw $t3, CENTR_BTTL          # $t3 = position of bottle opening
                         
-                        la $t7, ADDR_CAPSULE        # $t7 = location to store position of capsule
-                        la $t8, ADDR_COLOR_CAPSULE  # $t8 = location to colors of capsule
+                        la $t7, CAPSULE             # $t7 = location to store position of capsule
+                        la $t8, COLOR_CAPSULE       # $t8 = location to colors of capsule
                         sw $t3, 0($t7)              # save top capsule position in memory
                         sw $t4, 0($t8)              # save top capsule color in memory
                         addi $t3, $t3, 128
@@ -75,6 +88,105 @@ initialize_capsule:     addi $sp, $sp, -4           # put $ra onto stack
                         sw $t5, 4($t8)              # save bottom capsule color in memory
                         
                         jr $ra
+                        
+initialize_viruses:     addi $sp, $sp, -4                   # save $ra 
+                        sw $ra, 0($sp)                      #       on the stack
+                        
+                        jal generate_random_virus_position  # generate random virus positions
+                        jal generate_random_virus_position  # generate random virus positions
+                        jal generate_random_virus_position  # generate random virus positions
+                        jal generate_random_virus_position  # generate random virus positions
+                        
+                        la $t0, VIRUS_POSITIONS             # $t0 = address of array for virus positions
+                        
+                        lw $t1, 0($sp)                      # $t1 = position of virus 0
+                        addi $sp, $sp, 4            
+                        sw $t1, 0($t0)                      # save virus 0 position in memory
+                        
+                        lw $t1, 0($sp)                      # $t1 = position of virus 1
+                        addi $sp, $sp, 4            
+                        sw $t1, 4($t0)                      # save virus 1 position in memory
+                        
+                        lw $t1, 0($sp)                      # $t1 = position of virus 2
+                        addi $sp, $sp, 4            
+                        sw $t1, 8($t0)                      # save virus 2 position in memory
+                        
+                        lw $t1, 0($sp)                      # $t1 = position of virus 3
+                        addi $sp, $sp, 4            
+                        sw $t1, 12($t0)                     # save virus 3 position in memory
+                        
+                        jal random_color                    # generate random virus color
+                        jal random_color                    # generate random virus color                    
+                        jal random_color                    # generate random virus color
+                        jal random_color                    # generate random virus color
+                        
+                        la $t0, VIRUS_COLORS                # $t0 = address of array for virus colors
+                        
+                        lw $t1, 0($sp)                      # $t1 = color of virus 0
+                        addi $sp, $sp, 4            
+                        sw $t1, 0($t0)                      # save virus 0 color in memory
+                        
+                        lw $t1, 0($sp)                      # $t1 = color of virus 1
+                        addi $sp, $sp, 4            
+                        sw $t1, 4($t0)                      # save virus 1 color in memory
+                        
+                        lw $t1, 0($sp)                      # $t1 = color of virus 2
+                        addi $sp, $sp, 4            
+                        sw $t1, 8($t0)                      # save virus 2 color in memory
+                        
+                        lw $t1, 0($sp)                      # $t1 = color of virus 3
+                        addi $sp, $sp, 4            
+                        sw $t1, 12($t0)                     # save virus 3 color in memory
+                        
+                        lw $ra, 0($sp)                      # restore $ra
+                        addi $sp, $sp, 4                    #       from the stack
+                        
+                        jr $ra
+                        
+create_next_capsule:    addi $sp, $sp, -4           # put $ra onto stack
+                        sw $ra, 0($sp)
+                        jal random_color
+                        lw $t4, 0($sp)              # pop return value from stack
+                        addi $sp, $sp, 4
+                        jal random_color
+                        lw $t5, 0($sp)              # pop return value from stack
+                        addi $sp, $sp, 4
+                        
+                        lw $ra, 0($sp)              # restore return address
+                        addi $sp, $sp, 4
+                        
+                        la $t8, NEXT_CAPSULE_COLOR  # $t8 = location to colors of capsule
+                        sw $t4, 0($t8)              # save top capsule color in memory
+                        sw $t5, 4($t8)              # save bottom capsule color in memory
+                        
+                        jr $ra 
+                        
+generate_random_virus_position:     li $v0, 42              # 42 is system call code to generate random int
+                                    li $a0, 0               # $a0 is the lower bound
+                                    li $a1, 17              # $a1 is the upper bound
+                                    syscall                 # your generated number will be at $a0
+                                    sll $s0, $a0, 2         # $s0 = random x offset
+                                    
+                                    li $v0, 42              # 42 is system call code to generate random int
+                                    li $a0, 0               # $a0 is the lower bound
+                                    li $a1, 19              # $a1 is the upper bound
+                                    syscall                 # your generated number will be at $a0
+                                    sll $s1, $a0, 7         
+                                    addi $s1, $s1, 640      # $s0 = random y offset
+                                    
+                                    lw $t0, TOP_LEFT_BOTTLE # $t0 = top left internal corner of bottle
+                                    add $t0, $t0, $s0       # $t0 += x offset
+                                    add $t0, $t0, $s1       # $t0 += y offset
+                                    
+                                    addi $sp, $sp, -4       # save return
+                                    sw $t0, 0($sp)          #       value on stack
+
+                                    jr $ra
+                        
+##############################################################################
+                        
+                        
+################################## Drawers ###################################
     
 reset_screen:       lw $t0, ADDR_DSPL       # $t0 = start position
     
@@ -138,19 +250,49 @@ draw_border:        lw $t0, ADDR_DSPL                   # $t0 = base address for
                         sw $t2, 8($t3)
                     
                     jr $ra
+                    
+draw_viruses:       la $t0, VIRUS_POSITIONS     # $t0 = array pointer for virus positions
+                    la $t1, VIRUS_COLORS        # $t1 = array pointer for virus colors
+                    
+                    lw $t2, 0($t0)              # $t2 = position of virus 0
+                    lw $t3, 0($t1)              # $t3 = color of virus 0
+                    sw $t3, 0($t2)              # draw virus 0
+                    
+                    lw $t2, 4($t0)              # $t2 = position of virus 1
+                    lw $t3, 4($t1)              # $t3 = color of virus 1
+                    sw $t3, 0($t2)              # draw virus 1
+                    
+                    lw $t2, 8($t0)              # $t2 = position of virus 2
+                    lw $t3, 8($t1)              # $t3 = color of virus 2
+                    sw $t3, 0($t2)              # draw virus 2
+                    
+                    lw $t2, 12($t0)             # $t2 = position of virus 3
+                    lw $t3, 12($t1)             # $t3 = color of virus 3
+                    sw $t3, 0($t2)              # draw virus 3
+
+                    jr $ra
     
-draw_capsule:
-    la $t0, ADDR_CAPSULE        # $t0 = array of address of capsule position
-    la $t1, ADDR_COLOR_CAPSULE  # $t1 = array of address of capsule color
-    lw $t2, 0($t0)              # $t2 = top/left capsule position
-    lw $t3, 4($t0)              # $t3 = bottom/right capsule position
-    lw $t4, 0($t1)              # $t4 = top/left capsule color
-    lw $t5, 4($t1)              # $t5 = bottom/right capsule color
-    sw $t4, 0($t2)              # paint top of capsule
-    sw $t5, 0($t3)              # paint bottom of capsule
-    jr $ra
+draw_capsule:       la $t0, CAPSULE             # $t0 = array of address of capsule position
+                    la $t1, COLOR_CAPSULE       # $t1 = array of address of capsule color
+                    lw $t2, 0($t0)              # $t2 = top/left capsule position
+                    lw $t3, 4($t0)              # $t3 = bottom/right capsule position
+                    lw $t4, 0($t1)              # $t4 = top/left capsule color
+                    lw $t5, 4($t1)              # $t5 = bottom/right capsule color
+                    sw $t4, 0($t2)              # paint top of capsule
+                    sw $t5, 0($t3)              # paint bottom of capsule
+                    
+                    jr $ra
     
-RandomColor:
+draw_next_capsule:  lw $t0, NEXT_CAPSULE        # $t0 = next capsule top position
+                    la $t1, NEXT_CAPSULE_COLOR  # $t1 = array of address of next capsule color
+                    lw $t4, 0($t1)              # $t4 = top next capsule color
+                    lw $t5, 4($t1)              # $t5 = bottom next capsule color
+                    sw $t4, 0($t0)              # paint top of capsule
+                    sw $t5, 128($t0)            # paint bottom of capsule
+                    
+                    jr $ra
+    
+random_color:
     li $t0, 0xff0000            # $t0 = red
     li $t1, 0xffff00            # $t1 = yellow
     li $t2, 0x0000ff            # $t2 = blue
@@ -164,28 +306,43 @@ RandomColor:
     beq $a0, 0, Else1
     beq $a0, 1, Else2
     sw $t2, 0($sp)
-    j EndRandomColor
+    
+    jr $ra
     Else2: 
         sw $t1, 0($sp)
-        j EndRandomColor
+    jr $ra
     Else1: 
         sw $t0, 0($sp)
-EndRandomColor: jr $ra
+    jr $ra
 
-handle_keyboard_input:
-    lw $a0, 4($t0)                  # Load second word from keyboard
-    beq $a0, 0x71, exit             # Check if the key q was pressed
-    beq $a0, 0x61, a_key_press
-    beq $a0, 0x64, d_key_press
-    beq $a0, 0x73, s_key_press
-    beq $a0, 0x77, w_key_press
+##############################################################################
+                        
+                        
+############################## Event Handlers ################################
 
-    li $v0, 1                       # ask system to print $a0
-    syscall
-    b game_loop
-end_handle_keyboard_input:
+sleep:      li $v0, 32      # $v0 = system call for sleeping
+            li $a0, 66      # $a0 = time (in milliseconds) to sleep
+            syscall         # sleep
+            jr $ra
 
-a_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position pointers
+handle_keyboard_input:      addi $sp, $sp, -4               # save 
+                            sw $ra, 0($sp)                  #   return address
+
+                            lw $a0, 4($t0)                  # Load second word from keyboard
+                            beq $a0, 0x71, exit             # Check if the key q was pressed
+                            beq $a0, 0x61, a_key_press
+                            beq $a0, 0x64, d_key_press
+                            beq $a0, 0x73, s_key_press
+                            beq $a0, 0x77, w_key_press
+                            
+                            # li $v0, 1                       # ask system to print $a0
+                            # syscall
+                            
+end_handle_keyboard_input:  lw $ra, 0($sp)                  # restore
+                            addi $sp, $sp, 4                #   return address
+                            jr $ra
+
+a_key_press:        la $t0, CAPSULE                 # $t0 = capsule position pointers
                     lw $t2, 0($t0)                  # $t2 = top/left position
                     lw $t3, 4($t0)                  # $t3 = bottom/right position
                     
@@ -216,9 +373,9 @@ a_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position poi
                         sw $t2, 0($t0)                  # save new left position
                         sw $t3, 4($t0)                  # save new right position
                     
-end_a_key_press:    jr $ra
+end_a_key_press:    j end_handle_keyboard_input
 
-d_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position pointers
+d_key_press:        la $t0, CAPSULE                 # $t0 = capsule position pointers
                     lw $t2, 0($t0)                  # $t2 = top/left position
                     lw $t3, 4($t0)                  # $t3 = bottom/right position
                     
@@ -249,9 +406,9 @@ d_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position poi
                         sw $t2, 0($t0)                  # save new left position
                         sw $t3, 4($t0)                  # save new right position
                     
-end_d_key_press:    jr $ra
+end_d_key_press:    j end_handle_keyboard_input
 
-s_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position pointers
+s_key_press:        la $t0, CAPSULE                 # $t0 = capsule position pointers
                     lw $t2, 0($t0)                  # $t2 = top/left position
                     lw $t3, 4($t0)                  # $t3 = bottom/right position
                     
@@ -282,9 +439,9 @@ s_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position poi
                         sw $t2, 0($t0)                  # save new left position
                         sw $t3, 4($t0)                  # save new right position
                     
-end_s_key_press:    jr $ra
+end_s_key_press:    j end_handle_keyboard_input
 
-w_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position pointers
+w_key_press:        la $t0, CAPSULE                 # $t0 = capsule position pointers
                     lw $t2, 0($t0)                  # $t2 = top/left position
                     lw $t3, 4($t0)                  # $t3 = bottom/right position
                     
@@ -294,7 +451,7 @@ w_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position poi
                         lw $t5, 0($t5)                          # $t5 = color at position to the right of capsule
                         bne $t5, $zero, w_key_press_end         # if there if no space to rotate, then exit function
                     
-                        la $t1, ADDR_COLOR_CAPSULE      # $t1 = capsule color pointers
+                        la $t1, COLOR_CAPSULE           # $t1 = capsule color pointers
                         lw $t5, 0($t1)                  # $t5 = top color
                         lw $t6, 4($t1)                  # $t6 = bottom color
                         
@@ -314,4 +471,4 @@ w_key_press:        la $t0, ADDR_CAPSULE            # $t0 = capsule position poi
                         subi $t2, $t2, 128          # $t2 = new top position for capsule
                         sw $t2, 0($t0)              # save new top position
                         sw $t3, 4($t0)              # save new bottom position
-w_key_press_end:    jr $ra
+w_key_press_end:    j end_handle_keyboard_input
